48ae12fdd2810a4eab23e84f3207e0dce757d330
sharifrashed <sharif.rashed26@gmail.com>
2022-01-19 11:17:22 -0600

    finished login form and fixed nav bar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -7,7 +7,11 @@ export const NavBar = () => {
         <ul className="navbar">
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/customers">Customers</Link>
-                <Link className="navbar__link" to="/employeees">Employees</Link>
+            </li>
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/employees">Employees</Link>
+            </li>
+            <li className="navbar__item active">
                 <Link className="navbar__link" to="/tickets">Tickets</Link>
             </li>
         </ul>
